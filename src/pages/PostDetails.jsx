b19aa1e41f50d291e3708ac49e054f3a50f66ce8
najlaa-mohammad <najlaaajore2@gmail.com>
2025-07-17 17:09:00 +0300

    posts

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import "./PostDetails.css";
 
-
 const PostDetails = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
